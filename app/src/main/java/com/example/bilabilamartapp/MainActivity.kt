59b7d7d13d1903520796b0eedf394ea57cf9ac5f
Kelly <kelly-oyy@hotmail.com>
2024-09-17 21:03:53 +0800

    modify Color.kt and Theme.kt

--- app/src/main/java/com/example/bilabilamartapp/MainActivity.kt
+++ app/src/main/java/com/example/bilabilamartapp/MainActivity.kt
@@ -13,6 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bilabilamartapp.ui.theme.BilaBilaMartAppTheme
 
+//Admin module
+//-renew the membership tier
+//-update user reward
+//-CRUD
+//-create announcement
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
